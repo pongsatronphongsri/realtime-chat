773fd31886615fd1d4d6c570b5fdbd796bacef2c
Phongsatorn Phongsri <s6306021622011@email.kmutnb.ac.th>
2023-10-07 11:51:10 +0700

    edit

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 const express = require('express');
 const socketio = require('socket.io');
 const app = express();
-const crypto = require('crypto');
-
-app.set('view engine','ejs');
+const desCrypto = require('simple-des-crypto'); // Updated package
+const CryptoJS = require('crypto-js');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/',(req,res)=>{
-    res.render("index");
+app.get('/', (req, res) => {
+  res.render("index");
 });
 
-const server = app.listen(process.env.POST || 3000,()=>{
-    console.log("server is running..");
+const server = app.listen(process.env.POST || 3000, () => {
+  console.log("Server is running..");
 });
 
-const secretKey = crypto.randomBytes(32).toString('hex'); // Generate a random 256-bit (32-byte) key
+// Initialize socket for the server
+const io = socketio(server);
 
-// initiallize socket for the server
-const io = socketio(server)
-io.on('connection',(socket)=>{
-    console.log('New user connected');
+io.on('connection', (socket) => {
+  console.log('New user connected');
 
-    socket.username = "Anonymous"
-    socket.on("change_username",data=>{
-        socket.username = data.username
-    })
+  socket.username = "Anonymous";
+  socket.desKey = "12345678"; // Replace with your DES key (must be 8 characters)
 
-// handle    the new message event
-socket.on("new_message",data =>{
-    console.log("new message");
-    io.sockets.emit("receive_message",{message:data.message,username:socket.username})
-})
-socket.on('typing',data =>{
-    socket.broadcast.emit('typing',{username: socket.username})
-})
+  socket.on("change_username", (data) => {
+    socket.username = data.username;
+  });
+
+  // Handle the new message event
+  socket.on("new_message", (data) => {
+    console.log("New message");
+
+    // Encrypt the message using DES
+    const encryptedMessage = CryptoJS.DES.encrypt(data.message, socket.desKey).toString();
+
+    io.sockets.emit("receive_message", {
+      message: encryptedMessage,
+      username: socket.username
+    });
+  });
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', { username: socket.username });
+  });
 });
--- public/js/chatroom.js
+++ public/js/chatroom.js
@@ -1,10 +1,10 @@
 (function connect() {
-    let socket = io.connect("https://realtime-chat-x7ff.onrender.com/")
-    
-
+    let socket = io.connect("https://realtime-chat-x7ff.onrender.com/");
+    //let socket = io.connect("http://localhost:3000");
     let username = document.querySelector('#username');
     let usernameBtn = document.querySelector('#usernameBtn');
     let curUsername = document.querySelector('.card-header');
+    socket.desKey = "12345678";
 
     usernameBtn.addEventListener('click', e => {
         console.log(username.value);
@@ -25,10 +25,47 @@
     //
     socket.on('receive_message', data => {
         console.log(data)
-        let listItem = document.createElement('li')
-        listItem.textContent = data.username + " : " + data.message;
-        listItem.classList.add('list-group-item');
-        messageList.appendChild(listItem)
+        /*try {
+            // Decrypt the received message using DES
+            const bytes = CryptoJS.DES.decrypt(data.message, socket.desKey);
+            const decryptedMessage = bytes.toString(CryptoJS.enc.Utf8);
+    
+            if (decryptedMessage !== null && decryptedMessage !== undefined) {
+                let listItem = document.createElement('li');
+                listItem.textContent = data.username + " : " + decryptedMessage;
+                listItem.classList.add('list-group-item');
+                messageList.appendChild(listItem);
+              
+                
+            } else {
+                console.error("Decryption resulted in null or undefined message.");
+            }
+        } catch (error) {
+            console.error("Error decrypting message:", error);
+        }*/
+        try {
+            // Display the encrypted message
+            let encryptedListItem = document.createElement('li');
+            encryptedListItem.textContent = data.username + " (Encrypted): " + data.message;
+            encryptedListItem.classList.add('list-group-item');
+            document.getElementById('encrypted-message-list').appendChild(encryptedListItem);
+    
+            // Decrypt the received message using DES
+            const bytes = CryptoJS.DES.decrypt(data.message, socket.desKey);
+            const decryptedMessage = bytes.toString(CryptoJS.enc.Utf8);
+    
+            if (decryptedMessage !== null && decryptedMessage !== undefined) {
+                // Display the decrypted message
+                let decryptedListItem = document.createElement('li');
+                decryptedListItem.textContent = data.username + " (Decrypted): " + decryptedMessage;
+                decryptedListItem.classList.add('list-group-item');
+                messageList.appendChild(decryptedListItem);
+            } else {
+                console.error("Decryption resulted in null or undefined message.");
+            }
+        } catch (error) {
+            console.error("Error decrypting message:", error);
+        }
     })
     let info = document.querySelector('.info');
     message.addEventListener('keypress',e =>{
